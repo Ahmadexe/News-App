b1920f2f5bccdd0dd1e953ad7897a9dd9875b7f0
Ahmad <muahmad710@gmail.com>
2022-09-01 15:12:46 +0500

    Added API Service for home page

---
+++ lib/api/api_services.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:news_app/models/article.dart';
+
+class ApiServices {
+  var client = http.Client();
+  Future<List<Article>> getArticles() async {
+    List<Article> articles = [];
+    var uri = Uri.parse("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1138a2cde05a468d8db41f047d0cbf16");
+    var response = await client.get(uri);
+    var jsonData = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      jsonData['articles'].forEach((json){
+        if (json['url'] != null && json['urlToImage'] != null && json['description'] != null && json['content'] != null) {
+          Article article = Article.fromJson(json);
+          articles.add(article);
+        }
+      });
+      return articles;
+    } else {
+      throw Exception('Failed to load post');
+    }
+  }
+}
